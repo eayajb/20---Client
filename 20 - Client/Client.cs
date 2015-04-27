e517ec14da520f040f67e1d8bf5f2ab2f299c360
eayajb <psxjrbu@nottingham.ac.uk>
2015-04-27 16:09:06 +0100

    Upload

---
+++ 20 - Client/Client.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using ServerData;
+using Microsoft.Kinect;
+using System.Windows.Media.Media3D;
+
+namespace _20__Client
+{
+    class Client
+    {
+        public static string ip = "128.243.19.128";
+        public static Socket master;
+        public static string inputState = "z";
+        public static string CLIENT_ID = "CLIENT1";
+
+        public Client()
+        {
+            master = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(ip), 4242);
+
+            try
+            {
+                master.Connect( ipe );
+            }
+            catch
+            {
+                Console.WriteLine("COULD NOT CONNECT TO SERVER HOST!");
+                Thread.Sleep(1000);
+            }
+
+            Thread thread = new Thread(DataIN);
+            thread.Start();
+        }
+
+        static void DataIN()
+        {
+            byte[] buffer;
+            int readBytes;
+
+            for (; ; )
+            {
+                try
+                {
+                    buffer = new Byte[ master.SendBufferSize];
+                    readBytes = master.Receive(buffer);
+
+                    if (readBytes > 0)
+                    {
+                        DataManager(new Packet(buffer));
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine("Disconnected form Server");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        static void DataManager(Packet p)
+        {
+            // MANAGE RECEIVED DATA
+
+            switch (p.packetType)
+            {
+                case PacketType.InputCode:
+                    Console.WriteLine( "RECEIVED INPUT CODE: " + p.clientCode );
+                    if (p.clientCode == "exit")
+                        Environment.Exit(0);
+                    inputState = p.clientCode;
+                    break;
+            }
+        }
+
+        public void SendReferenceData( double[] referenceData )
+        {
+            Packet referenceDataPacket = new Packet( PacketType.RegisterClient, CLIENT_ID );
+            referenceDataPacket.SetReferenceData( referenceData );
+
+            SendPacket( referenceDataPacket );
+        }
+
+        public void SendInputCode( string inputCode )
+        {
+            Packet inputCodePacket = new Packet(PacketType.InputCode, CLIENT_ID);
+            inputCodePacket.clientCode = inputCode;
+
+            SendPacket( inputCodePacket );
+        }
+
+        Packet bodyDataPacket = null;
+        public void AddBodyData( double personID, Dictionary<JointType, Point3D> bodyDictIn, List<ColorSpacePoint> csPointsIn )
+        {
+            if (bodyDataPacket == null)
+                this.bodyDataPacket = new Packet( PacketType.Transfer, CLIENT_ID );
+            
+            this.bodyDataPacket.personList.Add( new Person( personID, csPointsIn, bodyDictIn ) );
+        }
+
+        public void SendBodyData()
+        {
+            if( this.bodyDataPacket != null )
+                SendPacket( this.bodyDataPacket );
+            this.bodyDataPacket = null;
+        }
+
+        public void SendPacket( Packet packet )
+        {
+            master.Send( packet.ToBytes() );
+        }
+    }
+}
---
+++ 20 - Client/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using _20__Client;
+using System.Windows.Media.Media3D;
+using System.ComponentModel;
+
+namespace _20___Client
+{
+    public partial class MainWindow : Window
+    {
+        Client clientHelper;
+
+        KinectSensor kinect = null;
+        MultiSourceFrameReader msfr = null;
+        FrameDescription colorFrameDescription = null;
+        IList<Body> bodies = null;
+
+        Canvas canvas;
+        double canvasWidth, canvasHeight;
+        double referenceFrameWidth, referenceFrameHeight, referenceFrameDepth;
+
+        public MainWindow()
+        {
+            this.clientHelper = new Client();
+
+            this.kinect = KinectSensor.GetDefault();
+            this.msfr = this.kinect.OpenMultiSourceFrameReader(FrameSourceTypes.Body);
+
+            msfr.MultiSourceFrameArrived += msfr_MultiSourceFrameArrived;
+
+            this.colorFrameDescription = this.kinect.ColorFrameSource.FrameDescription;
+
+            this.referenceFrameHeight = colorFrameDescription.Height;
+            this.referenceFrameWidth = colorFrameDescription.Width;
+            this.referenceFrameDepth = this.kinect.DepthFrameSource.DepthMaxReliableDistance;
+
+            kinect.Open();
+
+            Loaded += MainWindow_Loaded;
+
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.canvas = bodyPointsCanvas;
+            this.canvasWidth = canvas.Width;
+            this.canvasHeight = canvas.Height;
+
+            double[] referenceData = new double[10];
+            referenceData[0] = referenceFrameDepth;
+            referenceData[1] = referenceFrameHeight;
+            referenceData[2] = referenceFrameWidth;
+            referenceData[3] = canvasHeight;
+            referenceData[4] = canvasWidth;
+
+            this.clientHelper.SendReferenceData(referenceData);
+        }
+
+        string inputState;
+
+        private void msfr_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            ///READ INPUT STATE FROM CLIENT
+            inputState = Client.inputState;
+
+            ///DECLARE FRAMES
+            BodyFrame bodyFrame = null;
+
+            ///ACQUIRE AND VALIDATE FRAME
+            MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
+
+            if (multiSourceFrame == null)
+            {
+                return;
+            }
+
+            try
+            {
+                bodyFrame = multiSourceFrame.BodyFrameReference.AcquireFrame();
+
+                if ((bodyFrame == null))
+                {
+                    return;
+                }
+
+                ///PROCESS BODY DATA
+
+                this.bodies = new Body[bodyFrame.BodyCount];
+
+                ///REFRESH BODY DATA
+                bodyFrame.GetAndRefreshBodyData(this.bodies);
+
+                foreach (Body body in this.bodies)
+                {
+                    if (body != null)
+                    {
+                        if (body.IsTracked)
+                        {
+                            Dictionary<JointType, Point3D> tdPoints = new Dictionary<JointType, Point3D>();
+                            List<ColorSpacePoint> csPoints = new List<ColorSpacePoint>();
+
+                            foreach (JointType type in body.Joints.Keys)
+                            {
+                                Joint joint = body.Joints[type];
+                                Point3D point = new Point3D(joint.Position.X, joint.Position.Y, joint.Position.Z);
+                                ColorSpacePoint csp = this.kinect.CoordinateMapper.MapCameraPointToColorSpace(joint.Position);
+
+                                ///GET LIST OF JOINT POSITIONS
+                                tdPoints.Add(type, point);
+
+                                ///CANNOT BE SURE THERE WILL BE DATA IF "TRACKED" IS USED
+                                if (joint.TrackingState == TrackingState.Tracked)
+                                {
+                                    ///CALCULATE POSITION TO DRAW POINT
+                                    csPoints.Add(csp);
+                                }
+                            }
+
+                            DrawPoints(csPoints);
+
+                            ///TRANSFER DATA TO SERVER
+                            if (inputState == "t")
+                                this.clientHelper.AddBodyData( BiometricID(tdPoints), tdPoints, csPoints );
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                this.clientHelper.SendBodyData();
+                Client.inputState = "z";
+
+                ///DISPOSE
+                if (bodyFrame != null)
+                {
+                    bodyFrame.Dispose();
+                }
+            }
+        }
+
+        private double BiometricID(Dictionary<JointType, Point3D> bodyData)
+        {
+            double ID = 1;
+
+            /// CONSTRUCT ALGORITHM TO FIND A UNIQUE IDENTIFIER
+
+            return ID;
+        }
+
+        private void DrawPoints(List<ColorSpacePoint> bodyPoints)
+        {
+            canvas.Children.Clear();
+            foreach (ColorSpacePoint point in bodyPoints)
+            {
+                ///NEW ELLIPSE REQUIRED FOR EACH CHILD ELEMENT ADDED
+                Ellipse ellipse = new Ellipse
+                {
+                    Width = 20,
+                    Height = 20,
+                    Fill = Brushes.Red
+                };
+
+                if (point.X > 0 && point.Y > 0)
+                {
+                    ///CONVERT POSITION TO CANVAS
+                    Double convX = this.canvasWidth * (point.X / this.referenceFrameWidth);
+                    Double convY = this.canvasHeight * (point.Y / this.referenceFrameHeight);
+
+                    ///SET POSITION AND ADD TO CANVAS
+                    Canvas.SetLeft(ellipse, convX - (ellipse.Width / 2));
+                    Canvas.SetTop(ellipse, convY - (ellipse.Height / 2));
+
+                    canvas.Children.Add(ellipse);
+                }
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.msfr != null)
+            {
+                // MultiSourceFrameReder is IDisposable
+                this.msfr.Dispose();
+                this.msfr = null;
+            }
+
+            if (this.kinect != null)
+            {
+                this.kinect.Close();
+                this.kinect = null;
+            }
+        }
+    }
+}
